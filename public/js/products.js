bef5e84fcec7008c4bfa201959c3fc6415802ba7
Charles Lee <charleslee90@gmail.com>
2018-06-11 22:45:28 +0800

    Finishes responsibilities page

--- public/js/products.js
+++ public/js/products.js
@@ -1,9 +1,9 @@
 
-import '../css/about.css';
+import '../css/products.css';
 import { hotReload } from './util.js'
 
 window.addEventListener('load', function(e) {
-  console.log('--> Application started  |  Current route: /about')
+  console.log('--> Application started  |  Current route: /products')
 })
 
 hotReload();
