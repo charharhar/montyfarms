30fa7405e04302c8ede27bdc97f511fc07ca82f1
Charles Lee <charleslee@Charless-MacBook-Pro.local>
2018-06-20 17:15:34 +0800

    Completes 3d revolving about values section

--- public/js/about.js
+++ public/js/about.js
@@ -2,6 +2,8 @@
 import '../css/about.css';
 import {
   hotReload,
+  sliceArray,
+  findParent,
   mobileNavHandler,
   footerCopyrightYear,
 } from './util.js'
@@ -19,5 +21,82 @@ const hamburger = document.querySelector('.hamburger');
 const navigationListWrapper = document.querySelector('.navigation-list-wrapper');
 
 mobileNavHandler(hamburger, navigationListWrapper);
+
+/**
+ * 3D CAROUSEL HANDLER
+ */
+const valuesCarousel = document.querySelector('.values-carousel');
+const carouselSlides = sliceArray(valuesCarousel.querySelectorAll('.values-slide'));
+
+const MULTIPLIERS = [2, 1, 0, -1, -2]
+const SLIDES_TO_SHOW = 5;
+const theta = 24;
+const radius = 600;
+let currentMultiplier = 0;
+
+let currentIndex = 0;
+let activeSlides = [13, 14, 0, 1, 2];
+let activeIndexMapping = {}
+
+const updateIndexMapping = function() {
+  const tempIndexMapping = {}
+
+  activeSlides.forEach((slide, index) => {
+    tempIndexMapping[slide] = MULTIPLIERS[index]
+  })
+
+  activeIndexMapping = tempIndexMapping;
+}
+
+const handleSlideChange = function() {
+  carouselSlides.forEach(slide => {
+    if (activeSlides.indexOf(parseInt(slide.getAttribute('index'))) == -1) {
+      slide.style.opacity = 0;
+      slide.style.top = `${-9999}px`;
+    } else {
+      slide.style.opacity = 1;
+      slide.style.top = 0;
+    }
+  });
+
+  valuesCarousel.style.transform = `translateZ(-${radius}px) rotateY(${currentMultiplier * theta}deg) `
+}
+
+const handleSlideClick = function(e) {
+  const target = findParent(e.target, 'values-slide')
+  const nextIndex = parseInt(target.getAttribute('index'))
+  let nextActiveSlides;
+
+  for (let i = 0; i < SLIDES_TO_SHOW; i++) {
+    if (nextIndex === 0) {
+      nextActiveSlides = [13, 14, 0, 1, 2]
+    } else if (nextIndex === 1) {
+      nextActiveSlides = [14, 0, 1, 2, 3]
+    } else if (nextIndex === 13) {
+      nextActiveSlides = [11, 12, 13, 14, 0]
+    } else if (nextIndex === 14) {
+      nextActiveSlides = [12, 13, 14, 1, 0]
+    } else {
+      nextActiveSlides = [
+        nextIndex - 2,
+        nextIndex - 1,
+        nextIndex,
+        nextIndex + 1,
+        nextIndex + 2,
+      ]
+    }
+  }
+
+  currentIndex = nextIndex;
+  currentMultiplier = currentMultiplier + activeIndexMapping[currentIndex]
+
+  activeSlides = nextActiveSlides;
+  updateIndexMapping()
+  handleSlideChange()
+}
+
+updateIndexMapping()
+handleSlideChange();
+valuesCarousel.addEventListener('click', handleSlideClick)
 
 hotReload();
