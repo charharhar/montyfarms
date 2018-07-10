ee4c0c62e9ad34c50f263d32df041754cdaf6a45
Charles Lee <charleslee90@gmail.com>
2018-07-10 22:20:00 +0800

    Completes product section

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,8 @@
 
 import '../css/home.css';
 import {
+  scrollTo,
+  scrollTop,
   hotReload,
   sliceArray,
   mobileNavHandler,
@@ -28,8 +30,30 @@ mobileNavHandler(hamburger, navigationListWrapper);
  * SCROLL ANIMATION HANDLER
  */
 const waypoints = sliceArray(document.querySelectorAll('.waypoint'));
+const backToTop = document.querySelector('.backToTop');
+const scrollDownHint = document.querySelector('.scrollDownHint')
 
 window.addEventListener('scroll', e => handleAllAnimations(waypoints))
 window.addEventListener('load', e => handleAllAnimations(waypoints))
+
+window.addEventListener('scroll', e => {
+  if (scrollTop() > (window.outerHeight)) {
+    backToTop.classList.add('active');
+  } else {
+    backToTop.classList.remove('active');
+  }
+})
+
+backToTop.addEventListener('click', e => {
+  $('html, body').animate({
+    scrollTop: $('#homeTop-anchor').offset().top
+  }, 1000);
+})
+
+scrollDownHint.addEventListener('click', e => {
+  $('html, body').animate({
+    scrollTop: $('#scrollDownHint-anchor').offset().top
+  }, 1000);
+})
 
 hotReload();
--- public/js/products.js
+++ public/js/products.js
@@ -29,6 +29,12 @@ mobileNavHandler(hamburger, navigationListWrapper);
  * SLIDER HANDLER
  */
 slickHelper('#plantain-main', '#plantain-thumbnails')
+slickHelper('#cucumber-main', '#cucumber-thumbnails')
+slickHelper('#butternut-main', '#butternut-thumbnails')
+slickHelper('#onion-main', '#onion-thumbnails')
+slickHelper('#sweetpotato-main', '#sweetpotato-thumbnails')
+slickHelper('#pineapple-main', '#pineapple-thumbnails')
+slickHelper('#sweetpotatoflour-main', '#sweetpotatoflour-thumbnails')
 
 /**
  * SCROLL ANIMATION HANDLER
@@ -37,5 +43,103 @@ const waypoints = sliceArray(document.querySelectorAll('.waypoint'));
 
 window.addEventListener('scroll', e => handleAllAnimations(waypoints))
 window.addEventListener('load', e => handleAllAnimations(waypoints))
+
+/**
+ * PRODUCT BUTTON HANDLER
+ */
+const plantainBtn = document.querySelector('#plantainBtn');
+const cucumberBtn = document.querySelector('#cucumberBtn');
+const butternutBtn = document.querySelector('#butternutBtn');
+const onionBtn = document.querySelector('#onionBtn');
+const sweetpotatoBtn = document.querySelector('#sweetpotatoBtn');
+const pinappleBtn = document.querySelector('#pinappleBtn');
+const sweetpotatoflourBtn = document.querySelector('#sweetpotatoflourBtn');
+const buttonImg = sliceArray(document.querySelectorAll('.button-img'))
+
+const plantainContainer = document.querySelector('#plantainContainer');
+const cucumberContainer = document.querySelector('#cucumberContainer');
+const butternutContainer = document.querySelector('#butternutContainer');
+const onionContainer = document.querySelector('#onionContainer');
+const sweetpotatoContainer = document.querySelector('#sweetpotatoContainer');
+const pineappleContainer = document.querySelector('#pineappleContainer');
+const sweetpotatoflourContainer = document.querySelector('#sweetpotatoflourContainer');
+const productsContainer = sliceArray(document.querySelectorAll('.product-content-container'))
+
+plantainBtn.addEventListener('click', e => {
+  buttonImg.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  productsContainer.forEach(container => {
+    container.classList.remove('active');
+  })
+  plantainBtn.classList.add('active');
+  plantainContainer.classList.add('active');
+})
+
+cucumberBtn.addEventListener('click', e => {
+  buttonImg.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  productsContainer.forEach(container => {
+    container.classList.remove('active');
+  })
+  cucumberBtn.classList.add('active');
+  cucumberContainer.classList.add('active');
+})
+
+butternutBtn.addEventListener('click', e => {
+  buttonImg.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  productsContainer.forEach(container => {
+    container.classList.remove('active');
+  })
+  butternutBtn.classList.add('active');
+  butternutContainer.classList.add('active');
+})
+
+onionBtn.addEventListener('click', e => {
+  buttonImg.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  productsContainer.forEach(container => {
+    container.classList.remove('active');
+  })
+  onionBtn.classList.add('active');
+  onionContainer.classList.add('active');
+})
+
+sweetpotatoBtn.addEventListener('click', e => {
+  buttonImg.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  productsContainer.forEach(container => {
+    container.classList.remove('active');
+  })
+  sweetpotatoBtn.classList.add('active');
+  sweetpotatoContainer.classList.add('active');
+})
+
+pineappleBtn.addEventListener('click', e => {
+  buttonImg.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  productsContainer.forEach(container => {
+    container.classList.remove('active');
+  })
+  pineappleBtn.classList.add('active');
+  pineappleContainer.classList.add('active');
+})
+
+sweetpotatoflourBtn.addEventListener('click', e => {
+  buttonImg.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  productsContainer.forEach(container => {
+    container.classList.remove('active');
+  })
+  sweetpotatoflourBtn.classList.add('active');
+  sweetpotatoflourContainer.classList.add('active');
+})
 
 hotReload();
