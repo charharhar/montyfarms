ac229448ae5868c23c2a735016f2441cc7d7344f
Charles Lee <charleslee90@gmail.com>
2018-07-15 23:08:48 +0800

    Fixes some bugs

--- controllers/home.js
+++ controllers/home.js
@@ -2,7 +2,7 @@ const path = require('path');
 const appRootDir = require('app-root-dir');
 
 exports.index = (req, res, assetsMap) => {
-  res.render(path.resolve(appRootDir.get(), './views/home.html'), {
+  res.render(path.resolve(appRootDir.get(), './views/index.html'), {
     assetsMap: assetsMap,
   });
 };
