d1494630c3cb7a9c68c5c95ce30d73512e2ccd51
Charles Lee <charleslee90@gmail.com>
2018-11-01 18:35:11 +0800

    Removes onion sections. Adds Meta tags

--- public/js/products.js
+++ public/js/products.js
@@ -54,7 +54,6 @@ window.addEventListener('load', () => {
   slickHelper('#plantain-main', '#plantain-thumbnails')
   slickHelper('#cucumber-main', '#cucumber-thumbnails')
   slickHelper('#butternut-main', '#butternut-thumbnails')
-  slickHelper('#onion-main', '#onion-thumbnails')
   slickHelper('#sweetpotato-main', '#sweetpotato-thumbnails')
   slickHelper('#pineapple-main', '#pineapple-thumbnails')
   slickHelper('#sweetpotatoflour-main', '#sweetpotatoflour-thumbnails')
@@ -85,7 +84,6 @@ window.addEventListener('load', e => handleAllAnimations(waypoints))
 const plantainBtn = document.querySelector('#plantainBtn');
 const cucumberBtn = document.querySelector('#cucumberBtn');
 const butternutBtn = document.querySelector('#butternutBtn');
-const onionBtn = document.querySelector('#onionBtn');
 const sweetpotatoBtn = document.querySelector('#sweetpotatoBtn');
 const pinappleBtn = document.querySelector('#pinappleBtn');
 const sweetpotatoflourBtn = document.querySelector('#sweetpotatoflourBtn');
@@ -94,7 +92,6 @@ const buttonImg = sliceArray(document.querySelectorAll('.button-img'))
 const plantain = document.querySelector('#plantain');
 const cucumber = document.querySelector('#cucumber');
 const butternut = document.querySelector('#butternut');
-const onion = document.querySelector('#onion');
 const sweetpotato = document.querySelector('#sweetpotato');
 const pineapple = document.querySelector('#pineapple');
 const sweetpotatoflour = document.querySelector('#sweetpotatoflour');
@@ -143,17 +140,6 @@ butternutBtn.addEventListener('click', e => {
   })
   butternutBtn.classList.add('active');
   butternut.classList.add('active');
-})
-
-onionBtn.addEventListener('click', e => {
-  buttonImg.forEach(btn => {
-    btn.classList.remove('active')
-  })
-  productsContainer.forEach(container => {
-    container.classList.remove('active');
-  })
-  onionBtn.classList.add('active');
-  onion.classList.add('active');
 })
 
 sweetpotatoBtn.addEventListener('click', e => {
